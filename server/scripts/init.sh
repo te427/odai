b2a4cb6a3c8cd4369dcb88dc3f38a331494f2925
Kieran Collery <deathbask3t@gmail.com>
2020-10-07 19:59:20 -0700

    Adds superuser automated creation

--- server/scripts/init.sh
+++ server/scripts/init.sh
@@ -4,4 +4,5 @@ python manage.py collectstatic --no-input
 
 python manage.py migrate --run-syncdb
 
-python scripts/su.py
+SU_CMD="import os; from django.contrib.auth.models import User; USERNAME = os.getenv('DJANGO_DB_ADMIN_USERNAME'); EMAIL = os.getenv('DJANGO_DB_ADMIN_EMAIL'); PASSWORD = os.getenv('DJANGO_DB_ADMIN_PASSWORD'); User.objects.create_superuser(username=USERNAME, email=EMAIL, password=PASSWORD).save()"
+echo $SU_CMD | python manage.py shell
